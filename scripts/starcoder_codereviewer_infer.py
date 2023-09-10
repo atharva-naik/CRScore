f683e0786b59c8c7ebb52055bcc3d551394a2f2b
atharva-naik <atharvanaik2018@gmail.com>
2023-09-10 01:23:05 -0400

    starcoder inference script

--- scripts/starcoder_codereviewer_infer.py
+++ scripts/starcoder_codereviewer_infer.py
@@ -1,6 +1,20 @@
+import os
 import re
+import json
+from tqdm import tqdm
 from text_generation import Client
-from src.datautils import read_jsonl
+
+def read_jsonl(path):
+    data = []
+    with open(path) as f:
+        for line in f:
+            try:
+                js = json.loads(line.strip())
+            except:
+                print("Error during reading json data.")
+                continue
+            data.append(js)
+    return data
 
 def remove_patch_header(code: str):
     # Define the regex pattern to match expressions like "@@ -53,7 +53,7 @@" or "@@ -12,8 +12,20 @@"
@@ -25,9 +39,28 @@ def generate_before_after_code_from_patch(patch: str):
         else:
             new_lines.append(line)
             old_lines.append(line)
+
     return "\n".join(old_lines), "\n".join(new_lines)
 
 if __name__ == "__main__":
     data = read_jsonl("./data/Comment_Generation/msg-test.jsonl")
     client = Client("http://tir-1-32:8880", timeout=60)
-    print(client.generate("def read_file(filename):\n", max_new_tokens=64, top_p=0.95, temperature=0.2, do_sample=True).generated_text)
+    preds = []
+    if os.path.exists("./experiments/StarCoder_ZeroShot/preds.jsonl"):
+        response = input("overwrite current preds (y/n)?")
+        if response.lower() == "n": exit()
+    open("./experiments/StarCoder_ZeroShot/preds.jsonl", "w")
+    for rec in tqdm(data):
+        patch = rec["patch"]
+        old_code, new_code = generate_before_after_code_from_patch(patch)
+        # commit message generation using StarCoder.
+
+        # prompt = f"<commit_before>{old_code}<commit_msg>text<commit_after>{new_code}<eos>"
+        prompt = f"<commit_before>{old_code}<commit_msg>"
+        completion = client.generate(prompt, max_new_tokens=64, top_p=0.95, temperature=0.0001, do_sample=True).generated_text
+        commit_msg = completion.split("<commit_after>")[0].strip()
+        with open("./experiments/StarCoder_ZeroShot/preds.jsonl", "a") as f:
+            f.write(json.dumps({
+                "gold": rec['msg'],
+                "pred": commit_msg,
+            })+"\n")
